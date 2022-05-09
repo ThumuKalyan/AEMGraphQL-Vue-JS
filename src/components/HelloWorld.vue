0117d264a64274b683bcc604bc18205e4ee2f06f
Kalyan Thumu <58558850+ThumuKalyan@users.noreply.github.com>
2022-05-09 11:15:44 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,75 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="hello"> 
+   <h1>{{heading}}</h1>
+   <div v-html=copy></div>
+   <img :src= testimage._authorUrl alt='dog' width =50%>
+   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      heading: '',
+      copy: {},
+      states: '',
+      isPageLoaded: false,
+      testimage : {}
+
+    }
+  },
+  created()  {     
+     var jsondata = JSON.stringify({
+  query: `{
+  globalmodelByPath(_path: "/content/dam/samplepoc-1") {
+    item {
+      heading
+      copy {
+        html
+        markdown
+        plaintext
+        json
+      }
+      states
+      image {
+        ... on ImageRef {
+          _path
+          _authorUrl
+        }
+      }
+    }
+  }
+}`,
+  variables: {}
+});
+
+var config = {
+  method: 'post',
+  url: 'http://localhost:4502/content/graphql/global/endpoint.json',
+  headers: {     
+    'Authorization': 'Basic YWRtaW46YWRtaW4=', 
+    'Content-Type': 'application/json',        
+  },
+  data : jsondata
+};
+
+axios(config)
+.then((response) => {  
+  console.log(response.data.data.globalmodelByPath.item.heading);
+  this.heading = response.data.data.globalmodelByPath.item.heading;
+  this.copy = response.data.data.globalmodelByPath.item.copy.html; 
+  this.testimage = response.data.data.globalmodelByPath.item.image;
+  console.log(this.testimage);
+  this.isPageLoaded = true;
+})
+.catch(function (error) {
+  console.log(error);
+})
   }
 }
 </script>
